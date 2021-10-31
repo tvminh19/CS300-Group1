6e31e1decc32ce8f86259f7e9b5d67c956bee914
tvminh19 <tvminh.it@gmail.com>
2021-10-31 20:38:47 +0700

    test - build login screen

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Minh has edited'),
+      home: const MyHomePage(title: 'Minh-2 has edited'),
     );
   }
 }
---
+++ lib/pages/login/login.dart
@@ -0,0 +1,1 @@
+//Minh had written this.
