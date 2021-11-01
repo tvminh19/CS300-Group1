0fcddfd164738a40f0fddb4000a78c8fb4b818e1
tvminh19 <tvminh.it@gmail.com>
2021-11-01 17:18:57 +0700

    test - build login screen

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Chat extends StatefulWidget {
-  const Chat({Key? key}) : super(key: key);
+class Chat extends StatelessWidget {
+  const Chat({ Key? key }) : super(key: key);
 
-  @override
-  _ChatState createState() => _ChatState();
-}
-
-class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Text('Chat'),
+      margin: const EdgeInsets.only(top: 90, right: 0, left: 0, bottom: 0),
+      child: Text('Chat'),
     );
   }
 }
