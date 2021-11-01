e0b30a66ada24ff9fc7486ba892e817d3be4cf33
tvminh19 <tvminh.it@gmail.com>
2021-11-01 20:34:53 +0700

    Build data model

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Chat extends StatelessWidget {
-  const Chat({ Key? key }) : super(key: key);
+  const Chat({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 90, right: 0, left: 0, bottom: 0),
-      child: Text('Chat'),
+      color: Colors.white,
+      child: const Text(
+        'Chat',
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
