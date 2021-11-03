92821cf5e207094f6debb7613c0dc1c227d0fb8d
tvminh19 <tvminh.it@gmail.com>
2021-11-03 09:43:34 +0700

    set up 50% for appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:paylo_chat/pages/add_friend/add_friend.dart';
+import 'package:paylo_chat/pages/chat/components/chatAppBar.dart';
+import 'package:paylo_chat/pages/friend/components/friendAppbar.dart';
+import 'package:paylo_chat/pages/personal/components/PersonalAppBar.dart';
 import 'package:paylo_chat/pages/personal/personal.dart';
 
 import 'chat/chat.dart';
@@ -33,12 +36,16 @@ class _HomeState extends State<Home> {
     }
   }
 
+  final ChatAppBar _chatAppBar = ChatAppBar();
+  final FriendAppBar _friendAppBar = FriendAppBar();
+  final PersonalAppBar _personalAppBar = PersonalAppBar();
+
+  Widget _showAppbar = ChatAppBar();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Paylo! Chat'),
-      ),
+      appBar: FriendAppBar(),
       backgroundColor: Colors.white,
       body: _showPage,
       bottomNavigationBar: CurvedNavigationBar(
