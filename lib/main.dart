557543ed87f45bcfeb2b0c8d24f45d24692bc091
tvminh19 <tvminh.it@gmail.com>
2021-11-15 09:24:01 +0700

    build user-item 100%

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Home(),
     );
   }
 }
--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:paylo_chat/pages/chat/components/user_item.dart';
 
 class Chat extends StatelessWidget {
   const Chat({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('chat'),
-    );
+    return ListView(children: [
+      SizedBox(
+        width: 350,
+        child: TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            suffixIcon: const Icon(Icons.password),
+            label: const Text(
+              'Search',
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ),
+      UserItem(
+          fullName: 'Trinh Van Minh',
+          lastMessage: 'What\'s going on?',
+          imageUrl: 'imageUrl',
+          messageNumer: 5),
+    ]);
   }
 }
